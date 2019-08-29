455a0c87781a5e14d7d9592bff17fe12a4f1a59d
Indunil1992 <indunil@adroitlogic.com>
2019-08-29 18:14:01 +0530

    ko

--- testgcpindunil/function.js
+++ testgcpindunil/function.js
@@ -1,4 +1,29 @@
-exports.handler = function(request, response) {
-    console.log("test round C scheduler 1")
-    response.send({"message": "Successfully executed S1"});
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const pubsub = google.pubsub('v1');
+
+exports.handler = function (request, response) {
+    pubsub.projects.topics.subscriptions.list({
+        topic: `projects/${process.env.GCP_PROJECT}/topics/cloud-builds`,
+        pageSize: 10
+    })
+        .then(response => {
+            console.log(response.data); 
+            console.log("succccccssss") // successful response
+            /*
+            response.data = {
+                "subscriptions": [
+                    "projects/<project>/subscriptions/<subscription-1>",
+                    "projects/<project>/subscriptions/<subscription-2>",
+                    ...
+                ]
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); 
+             console.log("errorrrr")// an error occurred
+        });
+    
+    response.send({ "message": "Successfully executed S1" });
 }
