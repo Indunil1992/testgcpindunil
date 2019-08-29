898947e2ff52ffc3b21898b53403107de38b5685
Indunil1992 <indunil@adroitlogic.com>
2019-08-29 17:47:11 +0530

    12

--- testgcpindunil/function.js
+++ testgcpindunil/function.js
@@ -1,4 +1,4 @@
 exports.handler = function(request, response) {
     console.log("test round C scheduler 1")
-    response.send({"message": "Successfully executed"});
+    response.send({"message": "Successfully executed S1"});
 }
